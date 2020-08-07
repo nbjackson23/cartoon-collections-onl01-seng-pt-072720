9c276430ebc211e3746eda15fb9537265fd2bf54
Nancy Jackson <nbjackson23@gmail.com>
2020-08-07 17:19:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,14 @@ end
 summon_captain_planet(planeteer_calls)
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese# code an argument here
